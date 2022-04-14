3d5afeb27926647db546860cd666be6e081402fa
Daniel Bravo <daniel.brv@hotmail.cl>
2022-04-13 21:55:25 -0400

    modify bluetooth connection accept all devices

--- Frontend/src/components/BluetoothWifi.jsx
+++ Frontend/src/components/BluetoothWifi.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import '../styles/BluetoothWifi.scss'
 
-const conections = () => {navigator.bluetooth.requestDevice({filters:[{namePrefix:'LE'}]}).then(device => console.log(device))}
+const conections = () => {
+    navigator.bluetooth.requestDevice({
+        acceptAllDevices: true,
+        optionalServices: ['battery_service'] // Required to access service later.
+      })
+      .then(device => { /* … */ })
+      .catch(error => { console.error(error); });
+}
 
 const BluetoothWifi = () => {
   return (
     <ul>
         <button className="blue" onClick={conections} >Conectar Bluetooth</button>
-
     </ul>
-        
+    
 
       
      
