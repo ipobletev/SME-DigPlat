993a1bd84a191473c7be8a0521ccb327fea146b1
Daniel Bravo <daniel.brv@hotmail.cl>
2022-04-15 11:33:28 -0400

    Is combined upload image and OCR

---
+++ Frontend/src/components/UploadImageOCR.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { createWorker } from 'tesseract.js';
+
+window.addEventListener("paste", e => {
+  if (e.clipboardData.files.length > 0) {
+    const fileInput = document.querySelector("#myFile");
+
+    fileInput.files = e.clipboardData.files;
+
+    if (e.clipboardData.files[0].type.startsWith("image/")) {
+      setPreviwImage(e.clipboardData.files[0]);
+    }
+  }
+})
+
+function setPreviwImage(file) {
+  const fileReader = new FileReader();
+  
+  fileReader.readAsDataURL(file);
+  fileReader.onload = () => {
+    document.querySelector("#imagePreview").src = fileReader.result;
+  }
+  
+  const doOCR = async () => {
+    const worker = createWorker({
+      logger: m => console.log(m),
+    });
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data: { text } } = await worker.recognize(file);
+    document.getElementById("resultOCR").innerHTML = text;
+    
+  };
+  doOCR(); 
+  
+}
+
+const UploadImageOCR = () => {
+  return (
+    <div>
+        <form>
+            <input type="file" id="myFile" name="myFile" />
+            <button type="submit" >Upload</button>
+        </form>
+        <img alt="Preview" id="imagePreview" />
+        <div className="OCRtesseract">
+        <p id='resultOCR'>fasd</p>
+    </div>
+    </div>
+  );
+};
+
+export default UploadImageOCR;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Header from '../components/Header'
 import BluetoothWifi from '../components/BluetoothWifi'
 import OCRtesseract from '../components/OCRtesseract'
-import UploadImage from '../components/UploadImage'
+import UploadImage from '../components/UploadImageOCR'
+import UploadImageOCR from '../components/UploadImageOCR'
 
 const Home = () => {
     return (
         <div>
         <Header />
         <BluetoothWifi />
-        <OCRtesseract />
-        <UploadImage />
+        <UploadImageOCR />
         </div>
     )
 }
