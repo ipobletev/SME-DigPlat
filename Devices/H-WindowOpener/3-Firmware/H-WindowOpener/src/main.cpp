32d8a8c5eb2541f1137282285ceb4f1be6c026cc
Ismael Poblete <ismael.pbt@gmail.com>
2022-04-16 21:48:46 -0400

    add libraries and userconfig

--- Devices/H-WindowOpener/3-Firmware/H-WindowOpener/src/main.cpp
+++ Devices/H-WindowOpener/3-Firmware/H-WindowOpener/src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
+#include <userconfig.h>
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Init program");
 }
