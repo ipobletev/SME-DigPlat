802e96b8db6ac828a4d50e30a2752e5926e712bd
Ismael Poblete <ismael.pbt@gmail.com>
2022-04-16 21:48:06 -0400

    add hall sensor code

---
+++ Devices/H-WindowOpener/3-Firmware/H-WindowOpener/src/SME_hallsensor.cpp
@@ -0,0 +1,21 @@
+
+
+#include <Arduino.h>
+#include <SME_hallsensor.h>
+
+int Val1=0,Val2=0;             //Here you can store both values, the Val2 can be boolean
+
+void init_halls_sensors() {
+  Serial.begin(9600);
+  pinMode(Hall_Sensor_D,INPUT);
+
+}
+
+void readSensor() {
+  
+   Val1=analogRead(Hall_Sensor);            //We read both values and display them raw on the serial monitor
+   Serial.print(Val1);
+   Val2=digitalRead(Hall_Sensor_D);
+   Serial.print("\t");
+   Serial.println(Val2);
+}
---
+++ Devices/H-WindowOpener/3-Firmware/H-WindowOpener/src/SME_hallsensor.h
@@ -0,0 +1,10 @@
+
+#ifndef SME_HALLSENSOR_H
+#define SME_HALLSENSOR_H
+
+
+#define Hall_Sensor A0          //A0 used with analog output, D2 with digital output
+#define Hall_Sensor_D 2
+
+
+#endif
